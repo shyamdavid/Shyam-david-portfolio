09dcf0151e9c132e091b3bb845f125cdceafb5c7
shyam David <shyamdavid2003@gmail.com>
2025-07-12 12:16:53 +0530

    btn commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,7 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
 sr.reveal('.home__subtitle', {})
+sr.reveal('.home_button', {})
 sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
